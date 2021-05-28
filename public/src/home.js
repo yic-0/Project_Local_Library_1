65c0c58b9795062727f3a5e41a40bbb5b07fd241
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-05-27 22:58:26 -0500

    First solution to getMostCommonGenres()

--- public/src/home.js
+++ public/src/home.js
@@ -14,7 +14,22 @@ function getBooksBorrowedCount(books) {
   }, 0);
 }
 
-function getMostCommonGenres(books) {}
+function getMostCommonGenres(books) {
+  //This will be an array of genre objects
+  const genres = books.reduce((genres, book) => {
+    //genre = first object in genres who's name matches the genre of the current book
+    const genre = genres.find((genre) => genre.name === book.genre);
+    //If there is no genre with that name, then we create one with count 1, otherwise we increase that genre's count by one
+    !genre || genre === undefined
+      ? genres.push({ name: book.genre, count: 1 })
+      : genre.count++;
+    return genres;
+  }, []);
+  //sort the genre objects by their count, highest counts first
+  genres.sort((genre1, genre2) => genre2.count - genre1.count);
+  //return an array with just the first 5 elements of the sorted genres.
+  return genres.slice(0, 5);
+}
 
 function getMostPopularBooks(books) {}
 
