d39d5ad67f0593fdd527f3548ec4b7281ee95277
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-05-27 21:11:41 -0500

    first solution for PetitionBooksByBorrowedStatus() and addition of filterBorrowed helper function

--- public/src/books.js
+++ public/src/books.js
@@ -1,3 +1,5 @@
+const { getBooksBorrowedCount } = require("./home");
+
 function findAuthorById(authors, id) {
   return findElementById(authors, id);
 }
@@ -6,12 +8,22 @@ function findBookById(books, id) {
   return findElementById(books, id);
 }
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const returned = true;
+  const loaned = !returned;
+  const returnedBooks = filterBorrowed(books, returned);
+  const loanedBooks = filterBorrowed(books, loaned);
+  return [[...loanedBooks], [...returnedBooks]];
+}
 
 function getBorrowersForBook(book, accounts) {}
 
 function findElementById(elements, id) {
   return elements.find((element) => element.id === id);
+}
+
+function filterBorrowed(books, status) {
+  return books.filter(({ borrows }) => status === borrows[0].returned);
 }
 
 module.exports = {
