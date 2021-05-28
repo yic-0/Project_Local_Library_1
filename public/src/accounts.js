39086c298d2d1e1f43cd9b2972910f76c64f8317
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-05-27 19:49:03 -0500

    First solution for sortAccountsByLastName()

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -2,7 +2,16 @@ function findAccountById(accounts, id) {
   return accounts.filter((account) => account.id === id)[0];
 }
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  const _accounts = accounts.concat();
+  return _accounts.sort((acnt1, acnt2) => {
+    return acnt1.name.last === acnt2.name.last
+      ? 0
+      : acnt1.name.last < acnt2.name.last
+      ? -1
+      : 1;
+  });
+}
 
 function getTotalNumberOfBorrows(account, books) {}
 
