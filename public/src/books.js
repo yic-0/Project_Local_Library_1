c09cb19872e6d3157e21b6ee589d558c774a47db
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-05-27 20:46:56 -0500

    first solution for findBookById(), and updated findAuthorById() so that both would utilize the new helper function findElementById()

--- public/src/books.js
+++ public/src/books.js
@@ -1,12 +1,18 @@
 function findAuthorById(authors, id) {
-  return authors.find((author) => author.id === id);
+  return findElementById(authors, id);
 }
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return findElementById(books, id);
+}
 
 function partitionBooksByBorrowedStatus(books) {}
 
 function getBorrowersForBook(book, accounts) {}
+
+function findElementById(elements, id) {
+  return elements.find((element) => element.id === id);
+}
 
 module.exports = {
   findAuthorById,
