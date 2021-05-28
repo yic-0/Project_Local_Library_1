79c95116a92b4df37a2025b34878cc941120796f
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-05-27 23:09:43 -0500

    made a helper function for finding mostCommonItem, but I feel it's implementation is flawed.

--- public/src/home.js
+++ public/src/home.js
@@ -15,28 +15,34 @@ function getBooksBorrowedCount(books) {
 }
 
 function getMostCommonGenres(books) {
-  //This will be an array of genre objects
-  const genres = books.reduce((genres, book) => {
-    //genre = first object in genres who's name matches the genre of the current book
-    const genre = genres.find((genre) => genre.name === book.genre);
-    //If there is no genre with that name, then we create one with count 1, otherwise we increase that genre's count by one
-    !genre || genre === undefined
-      ? genres.push({ name: book.genre, count: 1 })
-      : genre.count++;
-    return genres;
-  }, []);
-  //sort the genre objects by their count, highest counts first
-  genres.sort((genre1, genre2) => genre2.count - genre1.count);
-  //return an array with just the first 5 elements of the sorted genres.
-  return genres.slice(0, 5);
+  return mostCommonItem(books, "genre");
 }
 
-function getMostPopularBooks(books) {}
+function getMostPopularBooks(books) {
+  return mostCommonItem(books, "borrows");
+}
 
 function getMostPopularAuthors(books, authors) {}
 
 function arrayItemCount(item) {
   return item.length;
+}
+
+//helper function to find most common items, keep track of their counts, sort them at the end, and only return the top 5
+function mostCommonItem(books, itemName) {
+  const items = books.reduce((items, book) => {
+    //item = first object in items who's name matches the item of the current book
+    const item = items.find((item) => item.name === book[itemName]);
+    //If there is no item with that name, then we create one with count 1, otherwise we increase that item's count by one
+    !item || item === undefined
+      ? items.push({ name: book[itemName], count: 1 })
+      : item.count++;
+    return items;
+  }, []);
+  //sort the item objects by their count, highest counts first
+  items.sort((item1, item2) => item2.count - item1.count);
+  //return an array with just the first 5 elements of the sorted items.
+  return items.slice(0, 5);
 }
 
 module.exports = {
