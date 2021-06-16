ce6b4daa3f5dcb5c899b548afc66fab983f0b18d
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-06-16 13:47:16 -0500

    refactored more, mostly comments, and cleaned up getTotalNumberOfBorrows with destructuring

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -10,24 +10,22 @@ function sortAccountsByLastName(accounts) {
   sorted.sort((acnt1, acnt2) =>
     acnt1.name.last.toLowerCase() > acnt2.name.last.toLowerCase() ? 1 : -1
   );
-  /* //If we wish to destructure
+  return sorted;
+
+  /* //If we wish to destructure,  but I find this even messier and hard to read
   sorted.sort(({ name: { last: name1 } }, { name: { last: name2 } }) =>
     name1.toLowerCase() > name2.toLowerCase() ? 1 : -1
   );
+  return sorted
   /**/
-  return sorted;
 }
 
 function getTotalNumberOfBorrows(account, books) {
-  //returns a NUMBER that is a total count
-  //count is all times provided account ID appears in any books of the borrowed array
-  //Reduce methodology
-  //iterate through every book in books and acc will be total matches of account.id
-  //  /* Reduce methodology
+  //iterate through every book in books to see if the given account has checked out that book at least once, and tally up the results
   const accountId = account.id;
-  return books.reduce((totalBorrowed, book) => {
-    const borrowed = book.borrows;
-    if (borrowed.some((item) => item.id === accountId)) totalBorrowed++;
+  return books.reduce((totalBorrowed, { borrows }) => {
+    //if any of the records in borrows match the accountId, then we can increment our totalBorrowed
+    if (borrows.some((record) => record.id === accountId)) totalBorrowed++;
     return totalBorrowed;
   }, 0);
 }
@@ -36,10 +34,11 @@ function getBooksPossessedByAccount(account, books, authors) {
   //filter through each book in books
   return (
     books
-      //checking to see if the most recent transaction
+      //checking to see if the most recent transaction matches our account and that it hasn't been returned
       .filter(
         (book) => book.borrows[0].id === account.id && !book.borrows[0].returned
       )
+      //map through all the filtered books to add the author object to it
       .map((book) => {
         book["author"] = authors.find((author) => author.id === book.authorId);
         return book;
