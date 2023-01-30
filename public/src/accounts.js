ad24b9e1d47c104de0ae67cb5ee0fbd047d83f6b
yic-0 <123557664+yic-0@users.noreply.github.com>
2023-01-30 03:36:42 -0500

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,49 +1,97 @@
 function findAccountById(accounts, id) {
-  //return the account object with the matching ID
-  return accounts.find((account) => account.id === id);
+
+  
+  //- An array of account objects.
+  //- A string ID of a single account object.
+  
+  //use find function to match id
+let findID = accounts.find((account) => account.id === id);
+return findID;
 }
 
-function sortAccountsByLastName(accounts) {
-  //sort the objects alphabetically by last name key
-  //return the sorted array
-  const sorted = [...accounts];
-  sorted.sort((acnt1, acnt2) =>
-    acnt1.name.last.toLowerCase() > acnt2.name.last.toLowerCase() ? 1 : -1
-  );
-  return sorted;
+//------------------
 
-  /* //If we wish to destructure,  but I find this even messier and hard to read
-  sorted.sort(({ name: { last: name1 } }, { name: { last: name2 } }) =>
-    name1.toLowerCase() > name2.toLowerCase() ? 1 : -1
-  );
-  return sorted
-  /**/
+
+function sortAccountsByLastName(accounts) { 
+accounts.sort((accountA, accountB) => 
+               accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1 : -1 );
+ return accounts;
+  
 }
+
+
+
+//------------------
+
+
+
+
+// function getTotalNumberOfBorrows(account, books) {
+// const accountID = account.id;
+// let total = 0;
+//   books.forEach(book => book.borrows.forEach(borrow => accountID === borrow.id && total++));
+//   return total;
+// }
+
 
 function getTotalNumberOfBorrows(account, books) {
-  //iterate through every book in books to see if the given account has checked out that book at least once, and tally up the results
   const accountId = account.id;
   return books.reduce((totalBorrowed, { borrows }) => {
-    //if any of the records in borrows match the accountId, then we can increment our totalBorrowed
     if (borrows.some((record) => record.id === accountId)) totalBorrowed++;
     return totalBorrowed;
   }, 0);
 }
 
+//------------------
+
+
+
 function getBooksPossessedByAccount(account, books, authors) {
-  //filter through each book in books
-  return (
-    books
-      //checking to see if the most recent transaction matches our account and that it hasn't been returned
-      .filter(
-        (book) => book.borrows[0].id === account.id && !book.borrows[0].returned
-      )
-      //map through all the filtered books to add the author object to it
-      .map((book) => {
-        book["author"] = authors.find((author) => author.id === book.authorId);
-        return book;
-      })
-  );
+
+  //It returns an array of book objects, including author information, that represents all books _currently checked out_ by the given account. _Look carefully at the object below,_ as it's not just the book object; the author object is nested inside of it.
+  
+  /*
+  [
+    {
+      id: "5f447132320b4bc16f950076",
+      title: "est voluptate nisi",
+      genre: "Classics",
+      authorId: 12,
+      author: {
+        id: 12,
+        name: {
+          first: "Chrystal",
+          last: "Lester",
+        },
+      },
+      borrows: [
+        {
+          id: "5f446f2e6059326d9feb9a68",
+          returned: false,
+        },
+        ...
+      ],
+    },
+  ]
+*/
+  
+  //obtain array of books possessed
+  let booksPossessed=[];
+  books.forEach(book => {
+    let borrowArray = book.borrows;
+    if (borrowArray.find(borrow => borrow.id === account.id && borrow.returned === false)) {
+      booksPossessed.push(book);
+    }
+  })
+  
+  //match to author
+  booksPossessed.forEach(book=>{
+    let author = authors.find(person => person.id === book.authorId);
+    book['author'] = author;
+  })
+  
+  return booksPossessed;
+  
 }
 
 module.exports = {
